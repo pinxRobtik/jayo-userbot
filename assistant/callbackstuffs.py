4f44387d2a25341ddc22723ebf75ec2697ea0513
pinxRobtik <129358594+pinxRobtik@users.noreply.github.com>
2023-12-29 21:17:29 +0700

    update callbackstuffs.py

--- assistant/callbackstuffs.py
+++ assistant/callbackstuffs.py
@@ -166,7 +166,7 @@ TOKEN_FILE = "resources/auths/auth_token.txt"
 )
 async def send(eve):
     key, name = (eve.data_match.group(1)).decode("UTF-8").split("_")
-    thumb = "https://graph.org/file/b0ede17600df06f798774.jpg"
+    thumb = "https://graph.org/file/24305705f2cb7ad791f16.jpg"
     await eve.answer("■ Sending ■")
     data = f"uh_{key}_"
     index = None
@@ -257,7 +257,7 @@ async def changes(okk):
     )
     cli = "\n\nKlik tombol di bawah untuk memperbarui!"
     if not match:
-        thumb = "https://graph.org/file/b0ede17600df06f798774.jpg"
+        thumb = "https://graph.org/file/24305705f2cb7ad791f16.jpg"
         try:
             if len(tl_chnglog) > 700:
                 tl_chnglog = f"{tl_chnglog[:700]}..."
